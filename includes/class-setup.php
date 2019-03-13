4a09ba9bf7555fabe924eff56817b7e3a8bea8f1
Adam Turner <adam.turner@wsu.edu>
2019-03-13 10:08:38 -0700

    :sparkles: Add user meta and profile field to save WSU NID
    Add a new form field on the user profile screen -- along with an
    update handler -- to allow users to manually save a WSU network
    ID to the user metadata to use with the accessibility checker
    (to override the email address in case the email address isn't
    a WSU email).

--- includes/class-setup.php
+++ includes/class-setup.php
@@ -137,6 +137,10 @@ class WSUWP_A11y_Status {
 		add_action( 'admin_notices', array( $this, 'user_a11y_status_notice__remind' ) );
 		add_action( 'admin_notices', array( $this, 'user_a11y_status_notice__action' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_action( 'edit_user_profile', array( $this, 'usermeta_form_field_nid' ) );
+		add_action( 'show_user_profile', array( $this, 'usermeta_form_field_nid' ) );
+		add_action( 'edit_user_profile_update', array( $this, 'usermeta_form_field_nid_update' ) );
+		add_action( 'personal_options_update', array( $this, 'usermeta_form_field_nid_update' ) );
 		add_filter( 'manage_users_columns', array( $this, 'add_a11y_status_user_column' ) );
 		add_filter( 'manage_users_custom_column', array( $this, 'manage_a11y_status_user_column' ), 10, 3 );
 		add_filter( 'user_row_actions', array( $this, 'add_a11y_status_user_row_action' ), 10, 2 );
@@ -889,6 +893,64 @@ class WSUWP_A11y_Status {
 		), $redirect_url );
 
 		return $redirect_url;
+	}
+
+	/**
+	 * Displays a field on the user profile screen to add a WSU NID.
+	 *
+	 * Callback method for the `edit_user_profile` and `show_user_profile`
+	 * hooks that allow adding fields and data to the user profile page for,
+	 * respectively, users viewing other user profiles and users viewing their
+	 * own profile.
+	 *
+	 * @since 0.9.0
+	 *
+	 * @param WP_User $user The WP_User object of the user being edited.
+	 * @return void
+	 */
+	public function usermeta_form_field_nid( $user ) {
+		?>
+		<h2>WSU Network ID</h2>
+		<table class="form-table">
+			<tbody>
+				<tr class="user-wsu-nid-wrap">
+					<th>
+						<label for="wsu_nid"><?php esc_html_e( 'WSU NID', 'wsuwp-a11y-status' ); ?></label>
+					</th>
+					<td>
+						<input type="text" name="wsu_nid" id="wsu_nid" aria-describedby="nid-description" value="<?php echo esc_attr( get_user_meta( $user->ID, '_wsu_nid', true ) ); ?>" class="regular-text">
+						<p class="description" id="nid-description">Enter a WSU Network ID if not using a WSU email address.</p>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<?php
+	}
+
+	/**
+	 * Handles data submitted from the WSU NID field on the user profile screen.
+	 *
+	 * Callback method for the `edit_user_profile_update` hook, which triggers
+	 * when a user submits data to update another user's profile, and the
+	 * `personal_options_update`, which triggers when a user submits data to
+	 * update their own profile.
+	 *
+	 * @since 0.9.0
+	 *
+	 * @param int $user_id Optional. The user ID of the user being edited.
+	 * @return int|bool Meta ID if a new key was created, or true if value was updated and false on failure or no change
+	 */
+	public function usermeta_form_field_nid_update( $user_id ) {
+		// Check permissions.
+		if ( ! current_user_can( 'edit_user', $user_id ) ) {
+			return false;
+		}
+
+		// Sanitize input data.
+		$wsu_nid = sanitize_text_field( wp_strip_all_tags( $_POST['wsu_nid'] ) );
+
+		// Create/update user metadata for the given user ID.
+		return update_user_meta( $user_id, '_wsu_nid', $wsu_nid );
 	}
 
 }
