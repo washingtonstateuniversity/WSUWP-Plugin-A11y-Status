1d9177e5ba83919080d5f50853f523e73c82d329
Adam Turner <act@adamturner.dev>
2021-07-21 11:33:25 -0700

    Bump required WP to 5.7

--- wsuwp-a11y-status.php
+++ wsuwp-a11y-status.php
@@ -7,7 +7,7 @@ Author: washingtonstateuniversity, Adam Turner
 Author URI: https://github.com/washingtonstateuniversity/
 Plugin URI: https://github.com/washingtonstateuniversity/wsuwp-plugin-a11y-status
 Text Domain: wsuwp-a11y-status
-Requires at least: 4.7
+Requires at least: 5.7
 Tested up to: 5.2.3
 Requires PHP: 7.0
 */
